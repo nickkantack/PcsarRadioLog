8ac41a60f36175c0d300315e3d2ce9d01addebb2
Nick Kantack <nickkantack@gmail.com>
2026-07-07 16:40:43 -0600

    Basic formatting for something usable for testing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SegmentEvent, TranscriptionEvent, TranscriptionEventType } from './constants';
 import { CommonModule } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'app-root',
@@ -10,7 +11,19 @@ import { CommonModule } from '@angular/common';
         RouterOutlet
     ],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
+    animations: [
+        trigger('expandCollapse', [
+            transition(':enter', [
+            style({ height: '0px', opacity: 0 }),
+            animate('300ms ease', style({ height: '*', opacity: 1 }))
+            ]),
+            transition(':leave', [
+            style({ height: '*', opacity: 1 }),
+            animate('300ms ease', style({ height: '0px', opacity: 0 }))
+            ])
+        ]),
+    ]
 })
 export class AppComponent {
     title = 'SAR Radio Log';
@@ -18,6 +31,10 @@ export class AppComponent {
     ws: WebSocket;
 
     segmentsShowing: SegmentEvent[] = [];
+
+    isAudioIncoming: boolean = false;
+    isConnectedToAudio: boolean = false;
+    connectionText: string = "Connecting to audio...";
 
     constructor() {
         this.ws = new WebSocket(`ws://${location.hostname}:3011/transcription_stream`);
@@ -27,9 +44,23 @@ export class AppComponent {
             switch (transcriptionEvent.type) {
                 case TranscriptionEventType.INITIALIZATION_EVENT:
                     // TODO anything to start transcription
+                    this.connectionText = "Connected";
+                    this.isConnectedToAudio = true;
+                    break;
+                case TranscriptionEventType.SPEECH_START_EVENT:
+                    this.isAudioIncoming = true;
+                    break;
+                case TranscriptionEventType.SPEECH_STOP_EVENT:
+                    this.isAudioIncoming = false;
+                    break;
+                case TranscriptionEventType.INFERENCE_START_EVENT:
+
+                    break;
+                case TranscriptionEventType.INFERENCE_STOP_EVENT:
+
                     break;
                 case TranscriptionEventType.SEGMENT_EVENT:
-                    this.segmentsShowing.push(transcriptionEvent as SegmentEvent);
+                    this.segmentsShowing.unshift(transcriptionEvent as SegmentEvent);
                     break;
                 default:
                     console.warn(`Unknown transcription event:`);
@@ -39,5 +70,9 @@ export class AppComponent {
         };
     }
 
+    trackByMessageContent(index: number, item: any) {
+        const entry: SegmentEvent = item as SegmentEvent;
+        return `${entry.time}:${entry.text}`;
+    }
 
 }
--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -11,6 +11,10 @@ export class TranscriptionEventType {
 
     static readonly SEGMENT_EVENT: string = "SegmentEvent";
     static readonly INITIALIZATION_EVENT: string = "InitializationEvent";
+    static readonly SPEECH_START_EVENT: string = "SpeechStartEvent";
+    static readonly SPEECH_STOP_EVENT: string = "SpeechStopEvent";
+    static readonly INFERENCE_START_EVENT: string = "InferenceStartEvent";
+    static readonly INFERENCE_STOP_EVENT: string = "InferenceStopEvent";
 
 }
 
