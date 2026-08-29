9ceef7147cec638a6741075f17521a1460f66179
nickkantack <nickkantack@gmail.com>
2026-08-29 10:45:18 -0600

    Docker compose packaged app runs

--- backend/common.py
+++ backend/common.py
@@ -7,18 +7,18 @@ def emit_to_transcriber(event):
     print(json.dumps(event), flush=True)
     
     try:
-        server_url = "http://127.0.0.1:8000/transcribe"
+        server_url = "http://transcriber:8000/transcribe"
         requests.post(server_url, json=event, timeout=0.5)
     except Exception as e:
         # Silently ignore errors sending to server to avoid breaking main flow
         pass
 
 
-def emit_to_websocket(event):
+def emit_to_server(event):
     print(json.dumps(event), flush=True)
     
     try:
-        server_url = "http://127.0.0.1:3011/event"
+        server_url = "http://server:3011/event"
         requests.post(server_url, json=event, timeout=0.5)
     except Exception as e:
         # Silently ignore errors sending to server to avoid breaking main flow
